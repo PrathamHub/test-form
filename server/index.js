ece716ba3f0e2bc9d109c93d142de72202dacd4e
PrathamHub <119048881+PrathamHub@users.noreply.github.com>
2024-05-08 17:25:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://test-form-frontend.vercel.app","https://test-form-frontend.vercel.app/register"],
+    origin: "https://test-form-frontend.vercel.app",
     methods: ["POST", "GET"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
