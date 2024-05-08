118f1b9aa0a5ef13eb964a34c7fbb25b3b5e10b2
PrathamHub <119048881+PrathamHub@users.noreply.github.com>
2024-05-08 16:57:35 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Form from "./pages/Form";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://test-form-api.vercel.app/";
 axios.defaults.withCredentials = true;
 function App() {
   return (
